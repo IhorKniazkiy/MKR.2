c558538a676da25d98a2c70849f4c0c846ae2a80
Kniazkyi <96521531+IhorKniazkiy@users.noreply.github.com>
2023-05-30 23:31:57 +0300

    Update mod.2.2..c

--- mod.2.2..c
+++ mod.2.2..c
@@ -3,24 +3,24 @@ void inputArraySize(int *n) {
     printf("Введіть кількість елементів масиву: ");
     scanf("%d", n);
 }
-void inputArray(float arr[], int n) {
+void inputArray(int arr[], int n) {
     printf("Введіть елементи масиву:\n");
     for (int i = 0; i < n; i++) {
-        scanf("%f", &arr[i]);
+        scanf("%d", &arr[i]);
     }
 }
-void outputArray(float arr[], int n) {
+void outputArray(int arr[], int n) {
     printf("Масив:\n");
     for (int i = 0; i < n; i++) {
-        printf("%.2f ", arr[i]);
+        printf("%d ", arr[i]);
     }
     printf("\n");
 }
-void sortArray(float arr[], int n) {
+void sortArray(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] < arr[j + 1]) {
-                float temp = arr[j];
+                int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
@@ -30,7 +30,7 @@ void sortArray(float arr[], int n) {
 int main() {
     int n;
     inputArraySize(&n);
-    float arr[n];
+    int arr[n];
     inputArray(arr, n);
     sortArray(arr, n);
     outputArray(arr, n);
